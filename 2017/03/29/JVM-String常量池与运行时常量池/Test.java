4f26254cae54d5891d30d81547239094a1bc541b
zhuopengcheng <zhuopengcheng@hotpu.cn>
2017-08-04 11:37:40 +0800

    Site updated: 2017-08-04 11:37:39

--- 2017/03/29/JVM-String常量池与运行时常量池/Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class Test{
-	public static String a = "a";
-	public static void main(){
-		String b = "b";
-		
-	}
-}
--- 2017/03/29/JVM-String常量池与运行时常量池/Test2.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-public class Test2{
-	public static final String str = "laji" + "MySQL";
-	public static void main(){	
-	}
-}
